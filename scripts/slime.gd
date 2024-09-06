870320ba6a39210521fc3450a6508388dc0f5d5c
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-06 19:22:25 +0100

    Add enemy

---
+++ scripts/slime.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+const SPEED = 45.0
+
+@onready var sprite: AnimatedSprite2D = $Sprite
+
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+
+var direction = Vector2.RIGHT
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# If we hit a wall, change direction. This is reflected in the sprite flip.
+	# NOTE: Ledges are not handled, and there is no gravity.
+	if ray_cast_left.is_colliding():
+		direction = Vector2.RIGHT
+		sprite.flip_h = false
+	elif ray_cast_right.is_colliding():
+		direction = Vector2.LEFT
+		sprite.flip_h = true
+
+	position += direction * SPEED * delta
