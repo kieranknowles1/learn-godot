316b61bdebd06965d12daa167c7f654c44f33382
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-06 20:41:32 +0100

    Add missing file

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,6 +3,7 @@ extends Area2D
 # GameManager must be unique across the scene. This kind of reference
 # only works when the node is in the same scene as the script.
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Count the max score on start
 func _ready() -> void:
@@ -11,6 +12,7 @@ func _ready() -> void:
 # Triggered when the player enters the coin's area.
 # The player has its own physics layer, which the coin filters to.
 func _on_body_entered(body: Node2D) -> void:
-	queue_free()
-
 	game_manager.add_point()
+
+	# Play the pickup animation, which handles sounds and destroys the coin.
+	animation_player.play("pickup")
