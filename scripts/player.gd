90830bc8402620bf9d2f82abd423f19f52095c51
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-06 19:36:09 +0100

    Use A/D/Space and controller to move

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,8 @@ const JUMP_VELOCITY = -300.0
 const COYOTE_TIME = 0.3
 var air_time = 0.0
 
+@onready var animation: AnimatedSprite2D = $Animation
+
 func can_jump() -> bool:
 	return air_time < COYOTE_TIME
 
@@ -24,13 +26,12 @@ func _physics_process(delta: float) -> void:
 		air_time = 0.0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and can_jump():
+	if Input.is_action_just_pressed("jump") and can_jump():
 		velocity.y = JUMP_VELOCITY
 		block_jump()
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
