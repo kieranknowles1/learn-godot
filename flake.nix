4269384d31ae6127f4b006fce2acd490eb72ff37
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-08 02:06:04 +0100

    Add dev shell

--- flake.nix
+++ flake.nix
@@ -3,16 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    nixcfg.url = "github:kieranknowles1/nixcfg";
+    nixcfg.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-      godot = pkgs.godot_4;
-    in {
+  outputs = { self, nixpkgs, nixcfg }: let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    godot = pkgs.godot_4;
+  in rec {
+    packages.x86_64-linux = {
       default = pkgs.writeShellScriptBin "editor" ''
-        exec ${pkgs.lib.getExe godot} --editor
+        exec ${pkgs.lib.getExe godot} --editor $@
       '';
+    };
+
+    devShells.x86_64-linux = {
+      default = let
+        openEditor = pkgs.lib.getExe packages.x86_64-linux.default;
+      # My extended version of mkShell, which preserves the user's shell of choice
+      in nixcfg.lib.shell.mkShellEx {
+        packages = [
+          godot
+        ];
+
+        shellHook = ''
+          # Run the editor in the background, discarding stdout and stderr
+          ${openEditor} > /dev/null 2>&1 &
+        '';
+      };
     };
   };
 }
