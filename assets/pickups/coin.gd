4108f758aa7ed51964b8a98cd22a421566029023
Kieran <kieranknowles11@hotmail.co.uk>
2024-09-14 14:52:23 +0100

    Move coin and add value var

--- assets/pickups/coin.gd
+++ assets/pickups/coin.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+@export var value = 1
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_collectable_component_collected(collector: Node) -> void:
 	if collector is not Player:
 		return
 
-	collector.add_points(1)
+	collector.add_points(value)
 
 	# Play the pickup animation, which handles sounds and destroys the coin.
 	animation_player.play("pickup")
